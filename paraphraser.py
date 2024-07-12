c29070c4352066aea76cece0e3bb6bfb621a96c4
Karthik-56 <karthiksurya611@gmail.com>
2024-07-13 03:33:51 +0530

    adding the source file

--- paraphraser.py
+++ paraphraser.py
@@ -8,17 +8,16 @@ st.header("Your content rephrasing assistance")
 contents = st.text_area("Please enter/paste your content:",height=250)
 
 #creating the style required to generate the response style of the email
-styles = st.selectbox("Style of rephrase",("Reriting","Expand and clarify","Condense and focus",
+styles = st.selectbox("Style of rephrase",("Rewriting","Expand and clarify","Condense and focus",
                                            "Adapt the tone","Abstracting","Acknowledging"), index=0)
 
 #creating the python function for generating the email response using LLM
 def paraphraser(contents,style):
     template = """ you are a expert english professional, who is skilled and wellversed in grammatical and rephrasing the content
-    of the paragraph and rephrase the content {content}  in the {style} style.
-    if any other questions or queries are asked please reply with the text- 'Sorry, i couldn't undestand you, apologies for the inconvinence
-caused,this scope of work may be beyond the currrent working level.' """
+    of the paragraph within 100 words and rephrase the content {contents}  in the {style} style.
+     """
     prompt_template = PromptTemplate.from_template(template=template)
-    prompt = prompt_template.format(context=contents,style=style)
+    prompt = prompt_template.format(contents=contents,style=style)
     llm = Ollama(model="llama3",temperature=0)
     response = llm(prompt)
     return response
